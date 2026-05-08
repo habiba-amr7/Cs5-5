2d1d2d2d11f5bcf2594e26ebf080d753d9721902
lekaa06 <lekaaashraf2024@gmail.com>
2026-05-08 19:44:03 +0300

    create the model and entity of movies project

---
+++ src/main/java/com/example/movies/model/Movie.java
@@ -0,0 +1,89 @@
+package com.example.movies.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String genre;
+    private int releaseYear;
+    private String description;
+    private String posterUrl;
+    private String videoUrl;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre, int releaseYear, String description, String posterUrl, String videoUrl) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.description = description;
+        this.posterUrl = posterUrl;
+        this.videoUrl = videoUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
